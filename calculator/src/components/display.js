7793ed84d09a2d0cdfcae9c22278c75aad39ebc9
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-09-22 11:02:34 -0500

    handles expressions, updates displayValue with result

--- calculator/src/components/display.js
+++ calculator/src/components/display.js
@@ -1,3 +1,5 @@
+/* eslint no-eval: 0 */
+
 import React, { Component } from 'react';
 import { Buttons } from './buttons'
 
@@ -6,12 +8,17 @@ export class Display extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      displayValue: '0'
+      displayValue: '0',
+      storedValue: '0',
+      operator: null
     }
     this.changeInput = this.changeInput.bind(this)
     this.clearInput = this.clearInput.bind(this)
+    this.setOperator = this.setOperator.bind(this)
+    this.runExpression = this.runExpression.bind(this)
   }
 
+  //Takes event.target.value as a prop from Button handleClick
   changeInput(e) {
     let newInput = e
     if(this.state.displayValue === '0') {
@@ -21,7 +28,7 @@ export class Display extends Component {
     }
     else {
       this.setState({
-        displayValue: this.state.displayValue + newInput
+        displayValue: this.state.displayValue + newInput      
       })
     }
   }
@@ -30,6 +37,26 @@ export class Display extends Component {
     this.setState({
       displayValue: '0'
     })
+  }
+
+  setOperator(op) {
+    console.log("op:", op);
+    this.setState({
+      displayValue: '',
+      operator: op,
+      storedValue: this.state.displayValue
+    })
+  }
+
+  runExpression() {
+    let total = eval(parseInt(this.state.storedValue,10) + this.state.operator + parseInt(this.state.displayValue, 10))
+    this.setState({
+      displayValue: total
+    })
+    console.log("this.state.storedValue:", this.state.storedValue); 
+    console.log("this.state.operator:", this.state.operator)
+    console.log("this.state.displayValue:", this.state.displayValue);
+    console.log("total:", total);
   }
 
 
@@ -50,19 +77,19 @@ export class Display extends Component {
               <Buttons type="6" value="6" onClick={this.changeInput}/>
               <Buttons type="5" value="5" onClick={this.changeInput}/>
               <Buttons type="4" value="4" onClick={this.changeInput}/>
-              <Buttons type="x"/>
+              <Buttons type="x" value="*" onClick={this.setOperator}/>
             </tr>
              <tr>
               <Buttons type="3" value="3" onClick={this.changeInput}/>
               <Buttons type="2" value="2" onClick={this.changeInput}/>
               <Buttons type="1" value="1" onClick={this.changeInput}/>
-              <Buttons type="%"/>
+              <Buttons type="%" value="/" onClick={this.setOperator}/>
             </tr>
              <tr>
               <Buttons type="0" value="0" onClick={this.changeInput}/>
-              <Buttons type="+"/>
-              <Buttons type="-"/>
-              <Buttons type="="/>
+              <Buttons type="+" value="+" onClick={this.setOperator}/>
+              <Buttons type="-" value="-" onClick={this.setOperator}/>
+              <Buttons type="=" onClick={this.runExpression}/>
             </tr>
           </tbody>
         </table>
